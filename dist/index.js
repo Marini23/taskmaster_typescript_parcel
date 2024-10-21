a87a03e42784113ff92aa108851aaa3afac0d21b
Maryna Udovychenko <marina.smile1m@gmail.com>
2024-10-21 22:50:42 +0200

    Delete dist directory

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("bootstrap/dist/css/bootstrap.min.css");
-function hello() {
-    console.log("hello, world");
-}
-hello();
-//# sourceMappingURL=index.js.map
