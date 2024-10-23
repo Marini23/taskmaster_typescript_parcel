36a5f1bee685026dc6f88f9d535871080c7f3187
Marini23 <marina.smile1m@gmail.com>
2024-10-23 23:17:24 +0200

    _add_open_modal

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,7 +1,38 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function hellotasks(): void {
-  console.log("hello, tasks");
-}
+const modal: HTMLElement = document.querySelector(
+  ".modal-content"
+) as HTMLElement;
+const overlay: HTMLDivElement = document.querySelector(
+  ".overlay"
+) as HTMLDivElement;
+const openModalBtn: HTMLButtonElement = document.querySelector(
+  ".btn-modal-open"
+) as HTMLButtonElement;
+// const closeModalBtn =
+//   document.querySelector<HTMLButtonElement>(".btn-modal-close");
 
-hellotasks();
+console.log(modal);
+const openModal = function (): void {
+  console.log("open");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+openModalBtn.addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+// closeModalBtn.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
