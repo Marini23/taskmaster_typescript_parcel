22370a3921410a6435ebc7afff2ce1054a0a1b97
Marini23 <marina.smile1m@gmail.com>
2024-11-16 22:45:57 +0100

    add_burger_menu

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const menuOpen = document.querySelector(".navbar-toggler") as HTMLButtonElement;
+
+const menuClose = document.querySelector(
+  ".btn-menu-close"
+) as HTMLButtonElement;
+
+const menuList = document.querySelector(".menu-list") as HTMLUListElement;
+
+menuOpen.addEventListener("click", (): void => {
+  menuList.classList.toggle("active");
+});
+
+menuClose.addEventListener("click", (): void => {
+  menuList.classList.toggle("active");
+});
