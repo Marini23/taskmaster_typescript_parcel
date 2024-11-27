2dc7eb4e5b07976cddf6be6efa87020f4050440a
Marini23 <marina.smile1m@gmail.com>
2024-11-27 21:01:39 +0100

    add_images_homePage

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
+
+const btnLoadMore: NodeListOf<HTMLButtonElement> =
+  document.querySelectorAll(".btn-read-more");
+
+btnLoadMore.forEach((button: HTMLButtonElement) => {
+  button.textContent = "Read More...";
+  button.addEventListener("click", (): void => {
+    const article = button.closest(".article-container") as HTMLElement;
+    const content = article?.querySelector(".article-load-more") as HTMLElement;
+
+    if (content) {
+      content.classList.toggle("article-load-more-open");
+      button.textContent = content.classList.contains("article-load-more-open")
+        ? "Read Less..."
+        : "Read More...";
+    }
+  });
+});
