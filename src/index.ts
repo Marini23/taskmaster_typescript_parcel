185f483ebe84627e0f233ce55d5c830b1cff7f47
Marini23 <marina.smile1m@gmail.com>
2024-12-03 23:21:38 +0100

    fix_animation

--- src/index.ts
+++ src/index.ts
@@ -3,64 +3,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const btnLoadMore: NodeListOf<HTMLButtonElement> =
   document.querySelectorAll(".btn-read-more");
 
-// btnLoadMore.forEach((button: HTMLButtonElement) => {
-//   button.textContent = "Read More...";
-//   console.log("1");
-//   button.addEventListener("click", (): void => {
-//     const article = button.closest(".article-container") as HTMLElement;
-//     const content = article?.querySelector(".article-load-more") as HTMLElement;
-
-//     if (content) {
-//       content.classList.toggle("open");
-//       button.textContent = content.classList.contains("open")
-//         ? "Read Less..."
-//         : "Read More...";
-//     }
-//   });
-// });
-
-btnLoadMore.forEach((button: HTMLButtonElement) => {
+btnLoadMore.forEach((button: HTMLButtonElement, index) => {
   button.textContent = "Read More...";
+
   button.addEventListener("click", (): void => {
     const article = button.closest(".article-container") as HTMLElement;
     const content = article?.querySelector(".article-load-more") as HTMLElement;
-    console.log("2");
-    console.log(content);
-
-    if (
-      content.classList.contains("opening") ||
-      content.classList.contains("open")
-    ) {
-      // If open, start closing animation
-      content.classList.remove("opening");
-      content.classList.add("closing");
-
-      // Remove the closing class after the animation ends
+    if (content) {
+      const dynamicHeight = content.scrollHeight;
+      content.style.setProperty("--dynamic-height", `${dynamicHeight}px`);
+      content.classList.toggle("article-load-more-open");
       content.addEventListener(
         "animationend",
         () => {
-          content.classList.remove("closing");
-          content.classList.remove("open"); // Fully hide
+          content.style.removeProperty("--dynamic-height");
         },
         { once: true }
       );
-    } else {
-      // If closed, calculate height and set the CSS variable
-      // content.style.setProperty("--full-height", content.scrollHeight + "px");
-      // console.log(content);
-      // Start opening animation
-      content.classList.remove("closing");
-      content.classList.add("opening");
-
-      // Add 'open' class when animation ends
-      content.addEventListener(
-        "animationend",
-        () => {
-          content.classList.remove("opening");
-          content.classList.add("open");
-        },
-        { once: true }
-      );
+      button.textContent = content.classList.contains("article-load-more-open")
+        ? "Read Less..."
+        : "Read More...";
     }
   });
 });
+
+
+
